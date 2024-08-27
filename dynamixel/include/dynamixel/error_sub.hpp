f51eb75d6ddecc4f05712f0f27c00d4b04e2990a
jogeonuuuu <wnrdjaos3@gmail.com>
2024-08-27 10:40:19 +0900

    Add files via upload

---
+++ dynamixel/include/dynamixel/error_sub.hpp
@@ -0,0 +1,29 @@
+#ifndef _ERRORSUB_HPP_
+#define _ERRORSUB_HPP_
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include "dynamixel/dxl.hpp"
+#include <memory>
+#include <chrono>
+#include <functional>
+#include <thread>
+
+using namespace std::chrono_literals;
+using std::placeholders::_1;
+
+class jetson : public rclcpp::Node 
+{
+private:
+    rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub;
+    std_msgs::msg::Int32 error;
+    bool tf = false;
+
+    Dxl dxl;
+
+    void sub_callback(const std_msgs::msg::Int32::SharedPtr msg);
+
+public:
+    jetson();
+    ~jetson();
+};
+#endif //_ERRORSUB_HPP_
